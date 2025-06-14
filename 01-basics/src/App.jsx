fa76bfe4f703fa51efa4bc238eb0b80b6f01a705
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2025-06-14 04:02:26 +0000

    Added first example

---
+++ 01-basics/src/App.jsx
@@ -0,0 +1,23 @@
+import waffle from "./waffle.jpg";
+import "./App.css"; // import CSS file to apply the style
+
+function App() {
+  // we can only return one single top-level JSX element
+  const luckyNumber = Math.floor(Math.random() * 100 + 1);
+  return (<>
+          <h1>Hello World</h1>
+          {/* font-size: 32px; font-family: Verdana */}
+          <h2 style={{
+            fontSize:"32px",
+            fontFamily:"Verdana"
+          }}>Goodbye World</h2>
+          <p>Your lucky number today is {luckyNumber}</p>
+          {/* This is for images saved in the ./src folder */}
+          <img src={waffle} style={{width:"50%"}}/>
+          {/* If the image is hosted elsewhere, not with the app, then the URL will suffice */}
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Root_beer_in_glass_mug.jpg/1200px-Root_beer_in_glass_mug.jpg"/>
+    </>)
+}
+
+// Make the App function available for other JavaScript file
+export default App;  
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 # React
+
+To create a React application:
+`npx create vite@latest`
